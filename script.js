45b39c2aa836e60f681e0ba58e761e890d4e3c99
Igor Josiel <igorjosielvitalino@gmail.com>
2026-01-10 17:25:38 -0300

    feature: Ao recarregar a página busca os livros armazenados no localstorage.

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const addBookButton = document.getElementById('add-book-button');
-const bookDialog = document.getElementById('book-dialog');
+const main = document.querySelector("main");
+
+/* Ao carregar a página, verifica se há livros armazenados no localStorage. */
+document.addEventListener("DOMContentLoaded", () => {
+  const books = JSON.parse(localStorage.getItem("books")) || [];
+
+  if (books.length === 0) {
+    const messageContainer = document.createElement("div");
+    messageContainer.className = "body-md pt-72";
+    main.appendChild(messageContainer);
+
+    const message = document.createElement("p");
+    message.className = "text-center w-full";
+    message.textContent = "Nenhum livro adicionado ainda. Comece adicionando seu primeiro livro!";
+
+    messageContainer.appendChild(message);
+  }
+});
+
+const addBookButton = document.getElementById("add-book-button");
+const bookDialog = document.getElementById("book-dialog");
 const cancelButton = document.getElementById("cancel-button");
 
-addBookButton.addEventListener('click', () => {
+addBookButton.addEventListener("click", () => {
   bookDialog.showModal();
 });
 
-cancelButton.addEventListener('click', () => {
+cancelButton.addEventListener("click", () => {
   bookDialog.close();
 });
